b5e845a716651bbd647690c164d6de5e82cff7a2
VilleHaapaniemi <ville.haapaniemi2@gmail.com>
2022-11-22 15:55:26 +0200

    Create next track div

--- frontend/src/components/NextTrack.jsx
+++ frontend/src/components/NextTrack.jsx
@@ -1,11 +1,21 @@
+import classes from "./NextTrack.module.css";
+import image from "../assets/Bahrain.png";
+
 const NextTrack = (props) => {
-    console.log(props.track)
-    return (
+  const track = props.track;
+
+  return (
+    <div className={classes.nextTrack}>
+      <h2>Next track</h2>
+      <div className={classes.flex}>
+        <img src={image} alt="Track"></img>
         <div>
-
+          <h3>{track.country}</h3>
+          <h3>{track.track_name}</h3>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default NextTrack;
