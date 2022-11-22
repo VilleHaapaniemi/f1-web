5050376857b8f7faaa661805711e8428e29ccd5c
VilleHaapaniemi <ville.haapaniemi2@gmail.com>
2022-11-22 22:16:44 +0200

    Track img added

--- frontend/src/components/NextTrack.jsx
+++ frontend/src/components/NextTrack.jsx
@@ -1,14 +1,15 @@
 import classes from "./NextTrack.module.css";
-import image from "../assets/Bahrain.png";
 
 const NextTrack = (props) => {
   const track = props.track;
+  const string = "Bahrain.png";
+  const trackImg = require(`../assets/${string}`);
 
   return (
     <div className={classes.nextTrack}>
       <h2>Next track</h2>
       <div className={classes.flex}>
-        <img src={image} alt="Track"></img>
+        <img src={trackImg} />
         <div>
           <h3>{track.country}</h3>
           <h3>{track.track_name}</h3>
