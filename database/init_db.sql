5fa932f0f5f04fc8be0b22f7c45c0bde0158dea4
VilleHaapaniemi <ville.haapaniemi2@gmail.com>
2023-01-08 21:59:29 +0200

    Skill values changed

--- database/init_db.sql
+++ database/init_db.sql
@@ -4,16 +4,16 @@ CREATE TABLE IF NOT EXISTS teams (
     team_skill_factor DECIMAL(4, 2) NOT NULL
 );
 INSERT INTO teams (team_name, team_skill_factor)
-VALUES ('Red Bull Racing', 1),
-    ('Ferrari', 1),
-    ('Mercedes', 1),
-    ('Alpine', 1),
-    ('McLaren', 1),
-    ('Alfa Romeo', 1),
-    ('Aston Martin', 1),
-    ('Haas F1 Team', 1),
-    ('AlphaTauri', 1),
-    ('Williams', 1);
+VALUES ('Red Bull Racing', 3),
+    ('Ferrari', 4),
+    ('Mercedes', 7),
+    ('Alpine', 10),
+    ('McLaren', 11),
+    ('Alfa Romeo', 16),
+    ('Aston Martin', 18),
+    ('Haas F1 Team', 30),
+    ('AlphaTauri', 22),
+    ('Williams', 40);
 
 CREATE TABLE IF NOT EXISTS drivers (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -30,26 +30,26 @@ INSERT INTO drivers (
         driver_skill_factor,
         team_id
     )
-VALUES ('Max', 'Verstappen', 1, 1),
-    ('Sergio', 'Perez', 1, 1),
-    ('Charles', 'Leclerc', 1, 2),
-    ('Carlos', 'Sainz', 1, 2),
-    ('Lewis', 'Hamilton', 1, 3),
-    ('George', 'Russel', 1, 3),
-    ('Esteban', 'Ocon', 1, 4),
-    ('Fernando', 'Alonso', 1, 4),
-    ('Lando', 'Norris', 1, 5),
-    ('Daniel', 'Ricciardo', 1, 5),
-    ('Valtteri', 'Bottas', 1, 6),
-    ('Zhou', 'Guanyu', 1, 6),
-    ('Lance', 'Stroll', 1, 7),
-    ('Sebastian', 'Vettel', 1, 7),
-    ('Kevin', 'Magnussen', 1, 8),
-    ('Mick', 'Schumacher', 1, 8),
-    ('Pierre', 'Gasly', 1, 9),
-    ('Yuki', 'Tsunoda', 1, 9),
-    ('Nicholas', 'Latifi', 1, 10),
-    ('Alexander', 'Albon', 1, 10);
+VALUES ('Max', 'Verstappen', 2, 1),
+    ('Sergio', 'Perez', 8, 1),
+    ('Charles', 'Leclerc', 3, 2),
+    ('Carlos', 'Sainz', 5, 2),
+    ('Lewis', 'Hamilton', 10, 3),
+    ('George', 'Russel', 9, 3),
+    ('Esteban', 'Ocon', 16, 4),
+    ('Fernando', 'Alonso', 10, 4),
+    ('Lando', 'Norris', 13, 5),
+    ('Daniel', 'Ricciardo', 23, 5),
+    ('Valtteri', 'Bottas', 19, 6),
+    ('Zhou', 'Guanyu', 22, 6),
+    ('Lance', 'Stroll', 50, 7),
+    ('Sebastian', 'Vettel', 25, 7),
+    ('Kevin', 'Magnussen', 40, 8),
+    ('Mick', 'Schumacher', 55, 8),
+    ('Pierre', 'Gasly', 27, 9),
+    ('Yuki', 'Tsunoda', 46, 9),
+    ('Nicholas', 'Latifi', 70, 10),
+    ('Alexander', 'Albon', 51, 10);
     
 CREATE TABLE IF NOT EXISTS tracks (
     id INT PRIMARY KEY AUTO_INCREMENT,
